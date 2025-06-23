412f792afc2fc93700ca1632366e59aa3f191a8e
Vlad <ciorescuvladstefan0@gmail.com>
2025-06-23 15:36:36 +0300

    you can ask the question trough the terminal and the so called LLM will answeare

--- main.py
+++ main.py
@@ -1,22 +1,43 @@
 import os
-
+import sys 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
+    
     load_dotenv()
-
     api_key = os.getenv("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    
+  
+    
+    if len(sys.argv) > 1:
+        prompt = sys.argv[1]
+    else:
+        prompt = "Prompt not provided"
+        sys.exit(1)
+    
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
+    
     response = client.models.generate_content(
          model = "gemini-2.0-flash-001", 
-         contents = prompt)
-    print("Prompt tokens:" , response.usage_metadata.prompt_token_count)
-    print("Response tokens:" , response.usage_metadata.candidates_token_count)
-    print("Response:")
-    print(response.text)
+         contents = messages)
+    
+    if "--verbose" in sys.argv:
+        print(f"User prompt: {prompt}")
+        print("Prompt tokens:" , response.usage_metadata.prompt_token_count)
+        print("Response tokens:" , response.usage_metadata.candidates_token_count)
+        print("Response:")
+        print(response.text)
+    else:
+        print("Response:")
+        print(response.text)
+    
+    
 
 
 
